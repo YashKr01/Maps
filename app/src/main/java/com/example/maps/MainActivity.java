6d230a5fc1865a00228899cc3ecca1550dad4f5d
yash <yashkumar201301@gmail.com>
2021-02-23 12:11:55 +0530

    Current Location

--- app/src/main/java/com/example/maps/MainActivity.java
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.maps;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.provider.Settings;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -22,24 +33,61 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private boolean isPermissionGranted;
-
+    private GoogleMap mGoogleMap;
+    private FloatingActionButton fab;
+    private FusedLocationProviderClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.fab);
+
         checkPermission();
 
+        initMap();
+
+        mLocationClient = new FusedLocationProviderClient(this);
+
+        fab.setOnClickListener(v ->
+                getCurrentLocation());
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+
+        mLocationClient.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Location location = task.getResult();
+                assert location != null;
+                gotoLocation(location.getLatitude(), location.getLongitude());
+            }
+        });
+
+    }
+
+    private void gotoLocation(double latitude, double longitude) {
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+    private void initMap() {
         if (isPermissionGranted) {
             SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.fragment);
             supportMapFragment.getMapAsync(this);
         }
-
     }
 
     private void checkPermission() {
@@ -48,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
                         isPermissionGranted = true;
                     }
 
@@ -71,10 +119,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        mGoogleMap = googleMap;
+//        mGoogleMap.setMyLocationEnabled(true);
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
